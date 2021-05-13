d1b752194a636c796de6497cb13367661589da25
kaushikgub <kaushik.gub@gmail.com>
2021-05-13 13:02:02 +0600

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Inventory;
+use App\Models\Sale;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $brands = Brand::all();
+        $brandStock = 0;
+        $brandCount = count($brands);
+        foreach ($brands as $brand) {
+            $brandStock += (int)$brand['stock'];
+        }
+
+        $inventoryStock = Inventory::query()->whereMonth('purchase_date', Carbon::now()->month)->sum('quantity');
+        $saleStock = Sale::query()->whereMonth('sale_date', Carbon::now()->month)->sum('quantity');
+        $userCount = User::query()->count() - 1;
+
+        return view('admin.dashboard.index', compact(
+                'brandStock',
+                'brandCount',
+                'inventoryStock',
+                'saleStock',
+                'userCount'
+            )
+        );
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,8 +1,93 @@
 @extends('layouts.app')
 @section('content')
     <div class="row">
-        <div class="col">
-            <h1>Dashboard</h1>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Brands</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $brandCount }}</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                                <i class="ni ni-money-coins"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-sm">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i></span>
+                        <span class="text-nowrap"><span class="text-danger">{{ number_format($brandStock, 2) }}</span> Total Stock</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Inventory</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ number_format($inventoryStock, 2) }} KG</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                                <i class="ni ni-chart-bar-32"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-sm">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i></span>
+                        <span class="text-nowrap">This month</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Sale</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ number_format($saleStock, 2) }} KG</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                                <i class="ni ni-active-40"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-sm">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i></span>
+                        <span class="text-nowrap">This month</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Users</h5>
+                            <span class="h2 font-weight-bold mb-0">{{ $userCount }}</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                                <i class="ni ni-chart-pie-35"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-sm">
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i></span>
+                        <span class="text-nowrap"><span class="text-danger">{{ $userCount }}</span> Active User</span>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
